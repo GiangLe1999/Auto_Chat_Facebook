89a90663b04f65d63e1b7ecf753aa957abf50066
Lê Thanh Giang <100833182+GiangLe1999@users.noreply.github.com>
2024-11-22 22:47:36 +0700

    Done for three accounts

--- nuoi-3-acc.py
+++ nuoi-3-acc.py
@@ -2,45 +2,52 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import random
 from conversation import conversation_script_2
 
+chrome_driver_path = r"C:\Workspace\Python\chromedriver.exe"
+
 # Cấu hình tài khoản
 accounts = {
-    # "A": {
-    #     "name": "1",
-    #     "chrome_path": "C:\\Others\\Tele Accounts\\84912161609\\GoogleChromePortable\\GoogleChromePortable.exe",
-    #     "user_data_dir": "C:\\Others\\Tele Accounts\\84912161609\\GoogleChromePortable\\Data\\profile\\Default",
-    #     "debug_port": 9224  # Cổng Remote Debugging riêng
-    # },
-    # "B": {
-    #     "name": "2",
-    #     "chrome_path": "C:\\Others\\Tele Accounts\\84816828974\\GoogleChromePortable\\GoogleChromePortable.exe",
-    #     "user_data_dir": "C:\\Others\\Tele Accounts\\84816828974\\GoogleChromePortable\\Data\\profile\\Default",
-    #     "debug_port": 9225  # Cổng Remote Debugging riêng
-    # },
     "A": {
-        "name": "3",
-        "chrome_path": "C:\\Others\\Tele Accounts\\84852158289\\GoogleChromePortable\\GoogleChromePortable.exe",
-        "user_data_dir": "C:\\Others\\Tele Accounts\\84852158289\\GoogleChromePortable\\Data\\profile\\Default",
-        "debug_port": 9226  # Cổng Remote Debugging riêng
-    },
-    "C": {
-        "name": "4",
-        "chrome_path": "C:\\Others\\Tele Accounts\\84914418511\\GoogleChromePortable\\GoogleChromePortable.exe",
-        "user_data_dir": "C:\\Others\\Tele Accounts\\84914418511\\GoogleChromePortable\\Data\\profile\\Default",
-        "debug_port": 9224  # Cổng Remote Debugging riêng
+        "name": "1",
+        "chrome_path": "C:\\Others\\Facebook Accounts\\caytienbmt05\\GoogleChromePortable\\GoogleChromePortable.exe",
+        "user_data_dir": "C:\\Others\\Facebook Accounts\\caytienbmt05\\GoogleChromePortable\\Data\\profile\\Default",
+        "debug_port": 9400  # Cổng Remote Debugging riêng
     },
     "B": {
-        "name": "5",
-        "chrome_path": "C:\\Others\\Tele Accounts\\84918134941\\GoogleChromePortable\\GoogleChromePortable.exe",
-        "user_data_dir": "C:\\Others\\Tele Accounts\\84918134941\\GoogleChromePortable\\Data\\profile\\Default",
-        "debug_port": 9225  # Cổng Remote Debugging riêng
-    }
+        "name": "2",
+        "chrome_path": "C:\\Others\\Facebook Accounts\\caytienbmt09\\GoogleChromePortable\\GoogleChromePortable.exe",
+        "user_data_dir": "C:\\Others\\Facebook Accounts\\caytienbmt09\\GoogleChromePortable\\Data\\profile\\Default",
+        "debug_port": 9401  # Cổng Remote Debugging riêng
+    },
+    "C": {
+        "name": "3",
+        "chrome_path": "C:\\Others\\Facebook Accounts\\legiangbmt017\\GoogleChromePortable\\GoogleChromePortable.exe",
+        "user_data_dir": "C:\\Others\\Facebook Accounts\\legiangbmt017\\GoogleChromePortable\\Data\\profile\\Default",
+        "debug_port": 9402  # Cổng Remote Debugging riêng
+    },
+    # "A": {
+    #     "name": "4",
+    #     "chrome_path": "C:\\Others\\Facebook Accounts\\lttskda\\GoogleChromePortable\\GoogleChromePortable.exe",
+    #     "user_data_dir": "C:\\Others\\Facebook Accounts\\lttskda\\GoogleChromePortable\\Data\\profile\\Default",
+    #     "debug_port": 9403  # Cổng Remote Debugging riêng
+    # },
+    # "B": {
+    #     "name": "5",
+    #     "chrome_path": "C:\\Others\\Facebook Accounts\\n17dcqt014\\GoogleChromePortable\\GoogleChromePortable.exe",
+    #     "user_data_dir": "C:\\Others\\Facebook Accounts\\n17dcqt014\\GoogleChromePortable\\Data\\profile\\Default",
+    #     "debug_port": 9404  # Cổng Remote Debugging riêng
+    # },
+    # "C": {
+    #     "name": "6",
+    #     "chrome_path": "C:\\Others\\Facebook Accounts\\thanhtruong1691\\GoogleChromePortable\\GoogleChromePortable.exe",
+    #     "user_data_dir": "C:\\Others\\Facebook Accounts\\thanhtruong1691\\GoogleChromePortable\\Data\\profile\\Default",
+    #     "debug_port": 9405  # Cổng Remote Debugging riêng
+    # },
 }
 
 # Hàm khởi tạo Selenium
@@ -58,33 +65,38 @@ def init_driver(account):
     options.add_argument("--disable-gpu") # Tắt GPU (tăng hiệu năng khi chạy headless)
 
     # Sử dụng webdriver-manager để tự động tải ChromeDriver
-    service = Service(ChromeDriverManager().install())
+    service = Service(chrome_driver_path)
     return webdriver.Chrome(service=service, options=options)
 
 def send_message(driver,  message):
     try:
-        # Chờ cho nhóm chat tải xong
+        # Chờ nhóm chat tải xong
         WebDriverWait(driver, 15).until(
-            EC.presence_of_element_located((By.XPATH, '//span[contains(@class, "peer-title")]'))
+            EC.presence_of_element_located((By.XPATH, '//span[contains(@class, "x9f619")]'))
         )
 
         # Chờ ô nhập liệu tin nhắn sẵn sàng
         message_box = WebDriverWait(driver, 15).until(
-            EC.element_to_be_clickable((By.XPATH, '//div[@contenteditable="true"]'))
+            EC.element_to_be_clickable((By.XPATH, '//div[@contenteditable="true" and @aria-label="Tin nhắn"]'))
         )
 
-         # Chờ thời gian ngẫu nhiên từ 30 đến 60 giây
+        # Chèn nội dung bằng JavaScript
+        message_box.click()
+        message_box.send_keys(message)
+
+        # Chờ thời gian ngẫu nhiên từ 30 đến 60 giây
         wait_time = random.randint(8, 15)
         print(f"Chờ {wait_time} giây trước khi gửi tin nhắn...")
         time.sleep(wait_time)
 
-        # Gửi tin nhắn
-        message_box.send_keys(message)
+        # Nhấn ENTER để gửi
         message_box.send_keys(Keys.ENTER)
         print(f"Đã gửi: {message}")
 
     except Exception as e:
         print(f"Lỗi khi gửi tin nhắn: {e}")
+        print("Debug HTML:")
+        print(driver.page_source)
 
 
 def scroll_to_bottom(driver):
@@ -104,7 +116,7 @@ def wait_for_new_message(driver, expected_message):
 
         # Lấy danh sách tất cả các thẻ <span> chứa tin nhắn trong nhóm
         messages = WebDriverWait(driver, 10).until(
-            lambda d: d.find_elements(By.XPATH, '//span[@class="translatable-message"]')
+            lambda d: d.find_elements(By.XPATH, '//div[@class="html-div xexx8yu x4uap5 x18d9i69 xkhd6sd x1gslohp x11i5rnm x12nagc x1mh8g0r x1yc453h x126k92a x18lvrbx"]')
         )
 
         # Duyệt qua từng tin nhắn để kiểm tra nội dung
@@ -130,7 +142,7 @@ def main():
     try:
         # Mở Telegram Web
         for driver in drivers.values():
-            driver.get("https://web.telegram.org/k/#-2043049770")
+            driver.get("https://www.facebook.com/messages/t/9331467936886129")
         time.sleep(5)
 
         # Duyệt qua kịch bản hội thoại
